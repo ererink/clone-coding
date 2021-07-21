658028fa851a711a1f04eb2aae3ece8dd2191998
yelkim0210 <87455535+yelkim0210@users.noreply.github.com>
2021-07-21 20:30:24 +0900

    Add files via upload
    return

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const calculator = {
-    add: function (a, b) {
-        console.log(a + b);
+    plus: function (a, b) {
+        return a + b;
     },
     minus: function (a, b) {
-        console.log(a - b);
+        return a - b;
+    },
+    times: function (a, b){
+        return a * b;
     },
     divide: function (a, b){
-        console.log(a / b);
+        return a / b;
     },
-    powerOf: function (a, b){
-        console.log(a * b);
+    power: function (a, b){
+        return a ** b;
     },
 };
 
-calculator.add(5,1);
-calculator.minus(5,1);
-calculator.divide(5,1);
-calculator.powerOf(5,1);
+const plusResult = calculator.plus(2, 3);
+const minusResult = calculator.minus(plusResult, 10);
+const timesResult = calculator.times(10, minusResult);
+const divideResult = calculator.divide(timesResult, plusResult);
+const powerResult = calculator.power(divideResult, minusResult);
+
+
+
