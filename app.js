606e08f247d1a338798938b55f39d9785459c5fd
yelkim0210 <87455535+yelkim0210@users.noreply.github.com>
2021-07-22 12:50:59 +0900

    Add files via upload
    if, else if, and (&&), || (or)

--- app.js
+++ app.js
@@ -1,26 +1,13 @@
-const calculator = {
-    plus: function (a, b) {
-        return a + b;
-    },
-    minus: function (a, b) {
-        return a - b;
-    },
-    times: function (a, b){
-        return a * b;
-    },
-    divide: function (a, b){
-        return a / b;
-    },
-    power: function (a, b){
-        return a ** b;
-    },
-};
+const age = parseInt(prompt("How old are you?"));
 
-const plusResult = calculator.plus(2, 3);
-const minusResult = calculator.minus(plusResult, 10);
-const timesResult = calculator.times(10, minusResult);
-const divideResult = calculator.divide(timesResult, plusResult);
-const powerResult = calculator.power(divideResult, minusResult);
-
-
-
+if(isNaN(age) || age < 0) {
+    console.log("Please write a real positive number");
+} else if (age < 18) {
+    console.log("You are too young.");
+} else if (age >= 18 && age <= 50) {
+    console.log("You can drink.");
+} else if (age > 50 && age <= 80) {
+    console.log("You should exercise.");
+} else if(age > 80){
+    console.log("You can do whatever you want.");
+}
