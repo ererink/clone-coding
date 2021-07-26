ec4638561a22dc4b851433564ab50b8aa8663b47
yelkim0210 <87455535+yelkim0210@users.noreply.github.com>
2021-07-26 12:37:20 +0900

    Add files via upload
    css in js, classList

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const title = document.querySelector(".hello h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-    console.log("title was clicked!");
+  const clickedClass = "clicked";
+  if (h1.classList.contains(clickedClass)) {
+      h1.classList.remove(clickedClass);
+  } else {
+      h1.classList.add(clickedClass);
+  }
 }
 
-title.addEventListener("click", handleTitleClick);
 
+h1.addEventListener("click", handleTitleClick);
